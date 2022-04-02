76cbca6db9aad6288b5ca7340a9a828532aa738e
Muhammed Rajab <muhammedrajab101@gmail.com>
2022-04-02 16:43:07 +0530

    Refactored the js file

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,67 +1,111 @@
 'use strict';
 const log = console.log;
 
-let borderNo = 1;
+const inputBox = document.querySelector('.search-box');
+const searchBtn = document.querySelector('.search-btn');
+
 const table = document.querySelector('.results-table');
 const tableBody = document.querySelector('.table-body');
 
-function renderBorderSharingCountryElement(data) {
-    const {
-        name:{common:  countryName},
-        flag: countryFlag,
-        population: countryPopulation
-    } = data;
-
-    const row = document.createElement('tr');
-    row.classList.add('table-row');
-
-    const tdClasses = ['country-no', 'country-name', 'country-flag', 'country-population'];
-    const tdInnerText = ['#'+borderNo, countryName, countryFlag, new Intl.NumberFormat().format(countryPopulation)];
-
-    tdClasses.forEach((class_, idx) => {
-        const td = document.createElement('td');
-        td.innerText = tdInnerText[idx];
-        td.classList.add(class_);
-        row.appendChild(td);
-    });
-
-    const countryMoreLinkTd = document.createElement('td');
-    countryMoreLinkTd.classList.add('country-more');
-    countryMoreLinkTd.innerHTML = `<a href="#" class="more-link"><i class="fa-solid fa-angles-right"></i></a>`;
-    row.appendChild(countryMoreLinkTd);
-
-    return row;
-};
-
-function renderBorderCountriesList(data) {
+class Renderer {
     
-    const {borders: countryBorders} = data;
+    static renderBorderSharingCountryElement(data, borderNo) {
+        const {name:{common:  countryName},flag: countryFlag,population: countryPopulation} = data;
+    
+        const row = document.createElement('tr');
+        row.classList.add('table-row');
+    
+        const tdClasses = ['country-no', 'country-name', 'country-flag', 'country-population'];
 
-    const allRequests = [];
+        const tdInnerText = ['#'+borderNo, countryName, countryFlag, new Intl.NumberFormat().format(countryPopulation)];
+    
+        tdClasses.forEach((class_, idx) => {
+            const td = document.createElement('td');
+            td.innerText = tdInnerText[idx];
+            td.classList.add(class_);
+            row.appendChild(td);
+        });
+    
+        const countryMoreLinkTd = document.createElement('td');
+        countryMoreLinkTd.classList.add('country-more');
+        countryMoreLinkTd.innerHTML = `<a href="#" class="more-link"><i class="fa-solid fa-angles-right"></i></a>`;
+        row.appendChild(countryMoreLinkTd);
+    
+        return row;
+    }
+}
 
-    countryBorders.forEach((border, idx) => {
-        fetch(`https://restcountries.com/v3.1/alpha/${border}`)
-        .then(response => {
-            allRequests.push(response);
-            return response.json();
-        })
-        .then(data => {
-            tableBody.appendChild(renderBorderSharingCountryElement(data[0]));
-        })
-        .finally(()=>borderNo++);
-    });
-
-    Promise.allSettled(allRequests)
-    .then(()=>{
-        table.style.opacity = 1;
-    });
-};
-
-function getCountrysNeighborData(countryName) {
+class App {
+    
     borderNo = 1;
-   fetch(`https://restcountries.com/v3.1/name/${countryName}`)
-    .then(response => response.json())
-    .then(data => renderBorderCountriesList(data[0]));
+    isSearchingForCountry = false;
+
+    constructor () {
+        this.currentCountry = "";
+        this.currentCountryData = {};
+        this.currentCountryNeighboursData = [];
+
+        // * Event listeners
+        searchBtn.addEventListener('click', () => {
+            
+            if (this.isSearchingForCountry) {
+                return;
+            };
+            
+            this.toggleSearchBtn();
+            this.toggleSearchState();
+            this.onNewCountrySearch(inputBox.value);
+        });
+    }
+
+    resetCurrentState() {
+        this.borderNo = 1;
+        tableBody.innerHTML = "";
+        this.currentCountryNeighbours = [];
+    }
+
+    toggleSearchBtn() {
+        searchBtn.disabled = !searchBtn.disabled;
+    }
+
+    toggleSearchState() {
+        this.isSearchingForCountry = !this.isSearchingForCountry;
+    }
+
+    onNewCountrySearch(countryName) {
+        this.resetCurrentState()
+        this.currentCountry = countryName;
+        this.__getCountrysNeighborData();
+    }
+
+    __getCountrysNeighborData() {
+        
+        fetch(`https://restcountries.com/v3.1/name/${this.currentCountry}`)
+         .then(response => response.json())
+         .then(data => {
+            [this.currentCountryData] = data;
+            this.__renderBorderCountriesList();
+         }).finally(()=>{
+            this.toggleSearchState();
+            this.toggleSearchBtn();
+        });
+    }
+
+    __renderBorderCountriesList() {
+    
+        const {borders: countryBorders} = this.currentCountryData;
+    
+        const allRequests = [];
+    
+        countryBorders.forEach(border => {
+            fetch(`https://restcountries.com/v3.1/alpha/${border}`)
+             .then(response => allRequests.push(response) && response.json())
+             .then(data => {
+                this.currentCountryNeighboursData.push(data[0]);
+                tableBody.appendChild(Renderer.renderBorderSharingCountryElement(data[0], this.borderNo));
+            }).finally(() => this.borderNo++);
+        });
+    }
 };
 
-getCountrysNeighborData("india");
+const app = new App();
