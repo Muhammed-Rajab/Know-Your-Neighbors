570691ae1023d4a15d7e358395b300fd6fadb8a0
Muhammed Rajab <muhammedrajab101@gmail.com>
2022-04-03 02:47:39 +0530

    Added nice animation 	- Needs to move toggleElement methods to different clss 	- Needs to add more validation

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,8 @@
 'use strict';
 const log = console.log;
 
+const rootEl = document.querySelector(':root');
+
 const inputBox = document.querySelector('.search-box');
 const searchBtn = document.querySelector('.search-btn');
 
@@ -14,6 +16,8 @@ class Renderer {
     
         const row = document.createElement('tr');
         row.classList.add('table-row');
+        row.classList.add('animate__animated');
+        row.classList.add('animate__fadeInDown');
     
         const tdClasses = ['country-no', 'country-name', 'country-flag', 'country-population'];
 
@@ -45,6 +49,9 @@ class App {
         this.currentCountryData = {};
         this.currentCountryNeighboursData = [];
 
+        // * Css variables
+        rootEl.style.setProperty("--table-body-loading-screen-visibility", "none");
+
         // * Event listeners
         searchBtn.addEventListener('click', () => {
             
@@ -54,18 +61,24 @@ class App {
             
             this.toggleSearchBtn();
             this.toggleSearchState();
+            this.toggleTableBodyLoadingScreen();
             this.onNewCountrySearch(inputBox.value);
         });
     }
 
     resetCurrentState() {
         this.borderNo = 1;
-        tableBody.innerHTML = "";
         this.currentCountryNeighbours = [];
+    }
+
+    toggleTableBodyLoadingScreen() {
+        rootEl.style.setProperty("--table-body-loading-screen-visibility", rootEl.style.getPropertyValue("--table-body-loading-screen-visibility") === "none" ? "block" : "none");
     }
 
     toggleSearchBtn() {
         searchBtn.disabled = !searchBtn.disabled;
+        searchBtn.querySelector('i').classList.toggle("fa-spinner");
+        searchBtn.querySelector('i').classList.toggle("loading-animation");
     }
 
     toggleSearchState() {
@@ -88,6 +101,7 @@ class App {
          }).finally(()=>{
             this.toggleSearchState();
             this.toggleSearchBtn();
+            this.toggleTableBodyLoadingScreen();
         });
     }
 
@@ -95,17 +109,29 @@ class App {
     
         const {borders: countryBorders} = this.currentCountryData;
     
-        const allRequests = [];
-    
         countryBorders.forEach(border => {
             fetch(`https://restcountries.com/v3.1/alpha/${border}`)
-             .then(response => allRequests.push(response) && response.json())
+             .then(response => response.json())
              .then(data => {
+                
+                 // * When first promise is fulfilled or rejected
+                if (this.borderNo === 1) tableBody.innerHTML = "";
+                
                 this.currentCountryNeighboursData.push(data[0]);
                 tableBody.appendChild(Renderer.renderBorderSharingCountryElement(data[0], this.borderNo));
-            }).finally(() => this.borderNo++);
+            })
+            .finally(() => {
+                this.borderNo++;
+
+                if (this.borderNo === countryBorders.length){
+                    // * When it's the last promise to be fulfilled or to be rejected
+                }
+            });
         });
     }
 };
 
 const app = new App();
+
+inputBox.value = "usa";
+searchBtn.click();
