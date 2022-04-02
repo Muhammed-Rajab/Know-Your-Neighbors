bfe29ddc70be2bee4312e02984488997bcf44c6a
Muhammed Rajab <muhammedrajab101@gmail.com>
2022-04-02 15:52:27 +0530

    Created basic application 	- CODE NEEDS REFACTORING INTO CLASSES

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,22 +1,22 @@
 'use strict';
 const log = console.log;
 
+let borderNo = 1;
+const table = document.querySelector('.results-table');
 const tableBody = document.querySelector('.table-body');
 
-function renderBorderSharingCountryElement(data, index) {
+function renderBorderSharingCountryElement(data) {
     const {
-        name:{
-            common:  countryName
-        },
+        name:{common:  countryName},
         flag: countryFlag,
         population: countryPopulation
     } = data;
 
     const row = document.createElement('tr');
-    row.classList.add('row');
+    row.classList.add('table-row');
 
     const tdClasses = ['country-no', 'country-name', 'country-flag', 'country-population'];
-    const tdInnerText = ['#'+index, countryName, countryFlag, new Intl.NumberFormat().format(countryPopulation)];
+    const tdInnerText = ['#'+borderNo, countryName, countryFlag, new Intl.NumberFormat().format(countryPopulation)];
 
     tdClasses.forEach((class_, idx) => {
         const td = document.createElement('td');
@@ -37,28 +37,31 @@ function renderBorderCountriesList(data) {
     
     const {borders: countryBorders} = data;
 
+    const allRequests = [];
+
     countryBorders.forEach((border, idx) => {
         fetch(`https://restcountries.com/v3.1/alpha/${border}`)
-        .then(response => response.json())
+        .then(response => {
+            allRequests.push(response);
+            return response.json();
+        })
         .then(data => {
-            log(data[0]);
-            tableBody.appendChild(renderBorderSharingCountryElement(data[0], idx + 1));
-        });
+            tableBody.appendChild(renderBorderSharingCountryElement(data[0]));
+        })
+        .finally(()=>borderNo++);
+    });
+
+    Promise.allSettled(allRequests)
+    .then(()=>{
+        table.style.opacity = 1;
     });
 };
 
 function getCountrysNeighborData(countryName) {
-    /*
-        TODO: Fetches data from countryName
-        TODO: Gets border countries if successful
-            TODO: Loop through borders and fetch their data
-            TODO: Store them incase if user wants to know more about em
-            TODO: Render the list to user
-        TODO: Else show error
-    */
+    borderNo = 1;
    fetch(`https://restcountries.com/v3.1/name/${countryName}`)
     .then(response => response.json())
     .then(data => renderBorderCountriesList(data[0]));
 };
 
-getCountrysNeighborData("germany");
+getCountrysNeighborData("india");
